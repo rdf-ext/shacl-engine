84c448d08f17a3c5d25da88438ff610bfcca91ac
bergi <bergi@axolotlfarm.org>
2023-02-21 23:44:15 +0100

    fix: use separate namespace for debug and trace severity

--- test/support/namespaces.js
+++ test/support/namespaces.js
@@ -5,6 +5,7 @@ const mf = namespace('http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#'
 const rdf = namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 const rdfs = namespace('http://www.w3.org/2000/01/rdf-schema#')
 const sh = namespace('http://www.w3.org/ns/shacl#')
+const shn = namespace('https://schemas.link/shacl-next#')
 const sht = namespace('http://www.w3.org/ns/shacl-test#')
 const xsd = namespace('http://www.w3.org/2001/XMLSchema#')
 
@@ -14,6 +15,7 @@ export {
   rdf,
   rdfs,
   sh,
+  shn,
   sht,
   xsd
 }
